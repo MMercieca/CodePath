2c372f7f57a6181f16114ef8d345b0919565c1ea
Matt Mercieca <mercieca@gmail.com>
2025-09-16 15:42:53 -0400

    Sign up, sign in, and sign out work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def sign_up
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable
+
+  def admin?
+    group_id == 3
+  end
+
+  def teacher?
+    group_id == 2 || admin?
+  end
 end
